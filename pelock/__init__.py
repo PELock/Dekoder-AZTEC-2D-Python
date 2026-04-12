79263d9eb48ca08fd1b8fe4881472599d9fa51c3
Bartosz Wójcik <support@pelock.com>
2026-04-12 23:13:57 +0200

    v1.1.0
    Zmodernizowana wersja SDK

---
+++ pelock/__init__.py
@@ -0,0 +1,7 @@
+"""PELock AZTEC 2D decoder (Web API client)."""
+
+from pelock.aztecdecoder import AZTecDecoder
+
+__version__ = "1.1.0"
+
+__all__ = ["AZTecDecoder", "__version__"]
--- pelock/aztecdecoder.py
+++ pelock/aztecdecoder.py
@@ -1,19 +1,17 @@
 #!/usr/bin/env python
 
 #
-#
 # Dekoder kodow AZTEC 2D z dowodow rejestracyjnych interfejs Web API
 #
-# Wersja         : AZTecDecoder v1.0
+# Wersja         : AZTecDecoder v1.1.0
 # Jezyk          : Python v3
 # Zaleznosci     : requests (https://pypi.python.org/pypi/requests/)
 # Autor          : Bartosz Wójcik (support@pelock.com)
 # Strona domowa  : https://www.dekoderaztec.pl | https://www.pelock.com
 #
 
-import struct
-import io
 import json
+from typing import Any, Dict, Optional
 
 # wymagany pakiet requests - instalacja komenda "pip install requests"
 import requests
@@ -30,15 +28,17 @@ class AZTecDecoder(object):
     #
     _api_key = ""
 
-    def __init__(self, api_key):
+    def __init__(self, api_key: str, timeout: float = 120.0) -> None:
         """Inicjalizacja klasy AZTecDecoder
 
         :param api_key: Klucz do uslugi WebApi
+        :param timeout: Limit czasu zapytania HTTP w sekundach (domyslnie 120)
         """
 
         self._api_key = api_key
+        self._timeout = timeout
 
-    def decode_text(self, text):
+    def decode_text(self, text: str) -> Optional[Any]:
         """Dekodowanie zaszyfrowanej wartosci tekstowej do
            wyjsciowej tablicy w formacie JSON.
 
@@ -52,7 +52,7 @@ class AZTecDecoder(object):
 
         return self.post_request(params)
 
-    def decode_text_from_file(self, text_file_path):
+    def decode_text_from_file(self, text_file_path: str) -> Optional[Any]:
         """Dekodowanie zaszyfrowanej wartosci tekstowej
            ze wskaznego pliku do wyjsciowej tablicy z
            formatu JSON.
@@ -72,7 +72,7 @@ class AZTecDecoder(object):
 
         return None
 
-    def decode_image_from_file(self, image_file_path):
+    def decode_image_from_file(self, image_file_path: str) -> Optional[Any]:
         """Dekodowanie zaszyfrowanej wartosci zakodowanej
            w obrazku PNG lub JPG/JPEG do wyjsciowej tablicy
            w formacie JSON.
@@ -86,7 +86,7 @@ class AZTecDecoder(object):
 
         return self.post_request(params)
 
-    def post_request(self, params_array):
+    def post_request(self, params_array: Dict[str, Any]) -> Optional[Any]:
         """Wysyla zapytanie POST do serwera WebApi
 
         :param params_array: Tablica z parametrami dla zapytania POST
@@ -105,18 +105,31 @@ class AZTecDecoder(object):
         # ustaw poprawnie element z plikiem
         if "image" in params_array:
 
-            files = {"image": open(params_array["image"], "rb")}
-            params_array.pop("image", None)
-
-            response = requests.post(self.API_URL, files=files, data=params_array)
+            image_path = params_array.pop("image", None)
+            with open(image_path, "rb") as image_file:
+                files = {"image": image_file}
+                response = requests.post(
+                    self.API_URL,
+                    files=files,
+                    data=params_array,
+                    timeout=self._timeout,
+                )
 
         else:
 
-            response = requests.post(self.API_URL, data=params_array)
+            response = requests.post(
+                self.API_URL, data=params_array, timeout=self._timeout
+            )
 
         if response and response.ok:
 
-            return json.loads(response.text)
+            try:
+
+                return json.loads(response.text)
+
+            except json.JSONDecodeError:
+
+                return None
 
         else:
 
